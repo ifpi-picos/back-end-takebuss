f11d93b48e6664df0e857856f92ffb7413752e16
ian <ianzoka763@gmail.com>
2023-10-25 09:04:49 -0500

    alguma coisa

--- src/app.js
+++ src/app.js
@@ -1,14 +1,22 @@
+const express = require('express');
 import Express from 'express';
 import logger from 'morgan';
 import rotasDePassageiro from './rotas/rotasDePassageiro.js';
-import rotasDeColaborador from './rotas/rotasDePassageiro.js';
+import rotasDeColaboradores from './rotas/rotasDePassageiros.js';
+
 
 const app = Express();
 app.use(logger('dev'));
+app.use(Express.json());
 
-app.use('/passageiro', rotasDePassageiro);
-app.use('/colaborador', rotasDeColaborador);
+
+app.get('/', (req, res) => {
+  res.send('Ok!');
+});
+
+app.use(rotasDeColaboradores);
+app.use(rotasDePassageiro);
 
 app.listen(3000, () => {
-  console.log('Server running on port 3000');
+  console.log('app listening on port 3000!');
 });
