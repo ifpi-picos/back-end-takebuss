8647261f1fb364289352f7f93c0839d72268e51e
ian gabriel <ianzpka763@gmail.com>
2023-11-06 07:19:02 -0500

    alterações antes de rodar

--- src/rotas/rotasDeColaborador.js
+++ src/rotas/rotasDeColaborador.js
@@ -1,9 +1,16 @@
 import Express from 'express';
+import { PrismaClient } from '@prisma/client';
 
 const router = Express.Router();
+const prisma = new PrismaClient();
 
-router.get('/', (req, res) => {
-  res.send('Get colaborador.');
+router.get('/', async (req, res) => {
+  const colaboradores = await prisma.colaborador.findMany({});
+  res.send(colaboradores)
+});
+router.get('/', async (req, res) => {
+  const passageiros = await prisma.passageiro.findMany({});
+  res.send(passageiros)
 });
 
 router.post('/', (req, res) => {
