4ee75c3b407a35e1c57b3f580105c92c37cf1e67
ian <ianzoka76@gmail.com>
2023-11-29 15:53:13 -0300

    atualizando projeto com autenticação de usuarios

--- src/app.js
+++ src/app.js
@@ -1,19 +1,21 @@
+import cors from 'cors';
 import Express from 'express'; // importa o módulo do express
 import logger from 'morgan';
-import rotasDePassageiro from './rotas/rotasDePassageiro.js';
-import rotasDeColaborador from './rotas/rotasDeColaborador.js';
+import aut from './middlewares/aut.js';
+import login from './rotas/login.js';
+import rotasDeUsuarios from './rotas/rotasDeUsuarios.js';
 
 // criar um instância/aplicação Express
 const app = Express();
 // middleware para converter os body da requisição para json
 app.use(Express.json());
-// app.use(primeiro);
-// app.use(segundo);
-
+app.use(cors());
 app.use(logger('dev'));
 
-app.use('/passageiro', rotasDePassageiro);
-app.use('/colaborador',rotasDeColaborador)
+app.use('/login', login);
+app.use(aut); // middleware de autenticacao
+//colocar abaixo todas as rotas privadas que precisam de autenticacao
+app.use('/usuarios', rotasDeUsuarios);
 
 app.listen(3000, () => {
   console.log('Server running on port 3000');
